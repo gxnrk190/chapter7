82d463020e178e8b8f031848e85ba954acba979b
root <root@localhost.localdomain>
2018-07-10 04:38:43 +0900

    Added new file

---
+++ screenshotter.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import win32gui, win32ui, win32con, win32api
+
+hdesktop = win32gui.GetDesktopWindow()
+
+width = win32api.GetSystemMetrics(win32.SM_CXVIRTUALSCREEN)
+height = win32api.GetSystemMetrics(win32.SM_CYVIRTUALSCREEN)
+left = win32api.GetSystemMetrics(win32.SM_XVIRTUALSCREEN)
+right = win32api.GetSystemMetrics(win32.SM_YVIRTUALSCREEN)
+
+desktop_dc = win32gui.GetWindowDC(hdesktop)
+img_dc= win32ui.CreateDCFromHandle(desktop_dc)
+
+mem_dc = img_dc.CreateCompatibleDC()
+
+screenshot = win32ui.CreateBitmap()
+screenshot.CreateCompatibleBitmap(img_dc, width, height)
+mem_dc.SelectObject(screenshot)
+
+mem_dc.BitBlt((0,0), (width,height), img_dc, (left, top), win32con.SRCCOPY)
+
+screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot.bmp')
+
+mem_dc.DeleteDC()
+win32gui.DeleteObject(screenshot.GetHandle())
